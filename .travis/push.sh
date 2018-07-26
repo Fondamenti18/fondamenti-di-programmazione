4041f9162fbec646f1d7ef8236f3233d16902ae8
Andrea Sterbini <a.sterbini@gmail.com>
2018-07-27 01:45:22 +0200

    TRAVIS

--- .travis/push.sh
+++ .travis/push.sh
@@ -2,5 +2,5 @@
 
 git add .
 git commit -m "TRAVIS"
-git push --repo https://${GITHUB_TOKEN}@github.com/Fondamenti18/fondamenti-di-programmazione &> /dev/null
+git push --repo https://${GITHUB_TOKEN}@github.com/Fondamenti18/fondamenti-di-programmazione.git &> /dev/null
 
