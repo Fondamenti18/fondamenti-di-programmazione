433a36222708fac7248ac7a54119bea87c0e34b8
Andrea Sterbini <a.sterbini@gmail.com>
2018-07-26 15:09:15 +0200

    Q2A

--- Makefile
+++ Makefile
@@ -1,18 +1,30 @@
 
+# variabili personalizzabili
+#
+TIMEOUT=60
+# TODO: aggiungere esercizio (oppure tenere gli esercizi in directory separate
+
+# environment
 SHELL:=/bin/bash
 PATH:=/opt/anaconda3/bin:$(PATH)
 
-TIMEOUT=60
-ULIMIT=ulimit -t $(TIMEOUT) -m 100000 -v 10000000 -f 10000
-RADON=radon cc -a -s --show-closures 
+# commands
+ULIMIT=ulimit -m 100000 -v 10000000 -f 10000
+RADON=radon cc -a -s --show-closures
+COG=python ../../../cc.py
+GRADE=PYTHONPATH=. python ../../../grade01.py
+TIMEIT=PYTHONPATH=../../..:. python -u -m timeit -c -v -v -v -v -r 10 -s 'import grade01' 'grade01.main()'
 
+# -u unbuffered I/O	-m module	-n numrun	-r numrepeat	-s startstatement	
+
+# files to produce
 STUDENTS=$(wildcard students/*/*/program01.py)
 TESTS:=$(STUDENTS:.py=.log)
 CYCLOMATIC:=$(STUDENTS:.py=.cyc)
 TIME:=$(STUDENTS:.py=.tim)
 COGNITIVE:=$(STUDENTS:.py=.cog)
 
-all: cyclomatic cognitive tests # time
+all: cyclomatic cognitive tests time
 
 tests:		$(TESTS)
 cyclomatic:	$(CYCLOMATIC)
@@ -22,18 +34,19 @@ time:		$(TIME)
 # TODO: cosa succede se fallisce? fa il popd o no? (oppure visto che è uno shell separato va bene?)
 
 %/program01.log: %/program01.py grade01.py
-	pushd $(dir $<) ; $(ULIMIT) ; PYTHONPATH=. python ../../../grade01.py &> program01.log ; popd
+	pushd $(dir $<) ; $(ULIMIT) -t $(TIMEOUT) ; $(GRADE) &> program01.log ; popd
 
 %/program01.cyc: %/program01.py grade01.py
-	pushd $(dir $<) ; $(RADON) program01.py > program01.cyc ; popd
+	pushd $(dir $<) ; $(RADON) program01.py &> program01.cyc ; popd
 	
 %/program01.cog: %/program01.py grade01.py
-	pushd $(dir $<) ; python ../../../cc.py program01.py > program01.cog ; popd
+	pushd $(dir $<) ; $(COG) program01.py &> program01.cog ; popd
 
 %/program01.tim: %/program01.py grade01.py
-	pushd $(dir $<) ; $(ULIMIT) ; python -u -m timeit -c -v -v -v -v -r 10 ../../../grade01.py > program01.tim ; popd
+	pushd $(dir $<) ; $(ULIMIT)  -t $(TIMEOUT)0 ; $(TIMEIT) &> program01.tim ; popd
 
 # TODO: raccogliere i valori in un unico JSON
+# TODO: commit dei file su github così la prossima volta eseguiamo solo i nuovi e non tutti
 
 clean:
 	rm -rf students/*/*/*.{log,cyc,tim,cog,csv} students/*/*/__*
--- grade01.py
+++ grade01.py
@@ -1,70 +1,78 @@
-
+import copy
 import unittest
 import os
 import importlib
 import json
+import program01 as program
 
-program=None
+#program=None
 
 class Test(unittest.TestCase):
+    def dotest01(self, lista, num, expected_lista, expected_result):
+        args = lista, num
+        orig = copy.deepcopy(args)
+        ret  = program.modi(*args)
+        self.__check(ret,   expected_result, orig, 'return sbagliato')
+        self.__check(lista, expected_lista,  orig, 'lista sbagliata')
+        return 1
 
     def test_program01_3(self):
-        #lista di valori piccoli max 3 cifre
-        lista=[70,330,293,154,128,113,178]
-        orig= [] + lista
-        ret= program.modi(lista,6)
-        self.__check(ret,   [293, 113],   orig, 'return')
-        self.__check(lista, [70,154,128], orig, 'ls')
+        "lista di valori piccoli max 3 cifre"
+        lista           = [70,330,293,154,128,113,178]
+        expected_lista  = [70, 154, 128]
+        expected_result = [293, 113]
+        num             = 6
+        return self.dotest01(lista, num, expected_lista, expected_result)
 
     def test_program01_7(self):
         "lista di valori tutti di 7 cifre"
-        lista=[1234579,1234604,1234613,1234641,1234684,1234687,1234793,1234836,1234837,1234847]
-        orig= [] + lista
-        ret= program.modi(lista,6)
-        self.__check(ret,   [1234613,1234687,1234837],   orig, 'return')
-        self.__check(lista, [1234579,1234641,1234793,1234847 ], orig, 'ls')
+        lista           = [1234579,1234604,1234613,1234641,1234684,1234687,1234793,1234836,1234837,1234847]
+        expected_lista  = [1234579,1234641,1234793,1234847 ]
+        expected_result = [1234613,1234687,1234837]
+        num             = 6
+        return self.dotest01(lista, num, expected_lista, expected_result)
             
     def test_program01_9(self):
         "lista di valori di max 9 cifre"
-        lista=[858659,8640829,777923,178433279,148035889,3125]
-        orig= [] + lista
-        ret= program.modi(lista,4)
-        self.__check(ret,   [],     orig, 'return')
-        self.__check(lista, [3125], orig, 'ls')
+        lista           = [858659,8640829,777923,178433279,148035889,3125]
+        expected_lista  = [3125]
+        expected_result = []
+        num             = 4
+        return self.dotest01(lista, num, expected_lista, expected_result)
 
     def test_program01_9_2(self):
         "lista di valori tutti da 9 cifre"
-        lista=[100000300, 100000431, 100000463, 100000647, 100000675, 100000687, 100001025, 100001111]
-        orig= [] + lista
-        ret= program.modi(lista,10)
-        self.__check(ret,   [100000463,100000687],     orig, 'return')
-        self.__check(lista, [100000431, 100000675, 100001111], orig, 'ls')
+        lista           = [100000300, 100000431, 100000463, 100000647, 100000675, 100000687, 100001025, 100001111]
+        expected_lista  = [100000431, 100000675, 100001111]
+        expected_result = [100000463,100000687]
+        num             = 10
+        return self.dotest01(lista, num, expected_lista, expected_result)
     
 
     def test_program01_10(self):
         "lista di valori di max 10 cifre con divisori grandi"
-        lista=[340887623,26237927,2491,777923,5311430407,6437635961,82284023]
-        orig= [] + lista
-        ret= program.modi(lista,4)
-        self.__check(ret, [26237927], orig, 'return')
-        self.__check(lista, [],       orig, 'ls')
+        lista           = [340887623,26237927,2491,777923,5311430407,6437635961,82284023]
+        expected_lista  = []
+        expected_result = [26237927]
+        num             = 4
+        return self.dotest01(lista, num, expected_lista, expected_result)
     
 
     def test_program01_5(self):
         "lista di valori tutti di 5 cifre"
-        lista=[12347,12369,13125, 13127,13202,13750,13751,13838,14406,14407,14421,24010,24019,24035,26364]
-        orig= [] + lista
-        ret= program.modi(lista,14)
-        self.__check(ret, [ 12347,  13127,  13751 , 14407, 24019], orig, 'return')
-        self.__check(lista, [ 12369,  13202,  13838,  14421, 24035 ],       orig, 'ls')
+        lista           = [12347,12369,13125, 13127,13202,13750,13751,13838,14406,14407,14421,24010,24019,24035,26364]
+        expected_lista  = [ 12369,  13202,  13838,  14421, 24035 ]
+        expected_result = [ 12347,  13127,  13751 , 14407, 24019]
+        num             = 14
+        return self.dotest01(lista, num, expected_lista, expected_result)
             
     def test_program01_11(self):
         "lista di valori tutti di 11 cifre"
-        lista=[10000000116, 10000000431, 10000000469, 10000000548, 10000000697, 10000000711, 10000000768, 10000000924]
-        orig= [] + lista
-        ret= program.modi(lista,16)
-        self.__check(ret, [10000000469,10000000711], orig, 'return')
-        self.__check(lista, [10000000116, 10000000548, 10000000768],       orig, 'ls')
+        lista           = [10000000116, 10000000431, 10000000469, 10000000548, 10000000697, 10000000711, 10000000768, 10000000924]
+        expected_lista  = [10000000116, 10000000548, 10000000768]
+        expected_result = [10000000469,10000000711]
+        num             = 16
+        return self.dotest01(lista, num, expected_lista, expected_result)
     
     def __check(self, value, expected, params=None, explanation=''):
         # TODO: add deepcopy of value to avoid side effects
@@ -76,14 +84,16 @@ class Test(unittest.TestCase):
             msg += "\t<- correct %s value" % explanation
         self.assertEqual(value, expected, msg)
 
-if __name__ == "__main__":
-    import program01 as program
+def main():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(Test))
     runner = unittest.TextTestRunner(verbosity=2)
     result = runner.run(suite)
     q2a=result.testsRun-len(result.failures)
     print('<q2a>'+json.dumps(q2a)+'</q2a>')   
+
+if __name__ == "__main__":
+    main()
 
     '''
     f = open(os.devnull,"w", encoding="utf8")
